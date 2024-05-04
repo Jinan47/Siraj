6b2a6261c3637c2169c2a5a22ed0db5819fb4535
Jinan47 <jinanmswi47@gmail.com>
2024-05-04 17:24:03 +0300

    cc

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -15,26 +15,22 @@ class About extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.only(top: 30),
           child: Column(children: [
-            Center(
-              child: createAboutText(
-                  "Welcome to Siraj, a thoughtfully crafted mobile application designed to enhance your experience with the Quran. Developed using the versatile Flutter framework, Siraj brings a unique approach to exploring the sacred verses of the Quran."),
-            ),
+            createAboutText(
+                "Welcome to Siraj, a thoughtfully crafted mobile application designed to enhance your experience with the Quran. Developed using the versatile Flutter framework, Siraj brings a unique approach to exploring the sacred verses of the Quran."),
             const SizedBox(height: 15),
-            Center(
-              child: createAboutText(
-                  "With powerful classification tools, it organizes verses into categories, simplifying navigation for users to delve into specific themes."),
-            ),
+            createAboutText(
+                "With powerful classification tools, it organizes verses into categories, simplifying navigation for users to delve into specific themes."),
             const SizedBox(height: 15),
-            Center(
-                child: createAboutText(
-                    "Siraj features dynamic charts for a visual representation of topic distribution and allows for personalized aesthetics."))
+            createAboutText(
+                "Siraj features dynamic charts for a visual representation of topic distribution and allows for personalized aesthetics.")
           ]),
         ));
   }
 }
 
 Widget createAboutText(String text) {
-  return Padding(
+  return Center(
+      child: Padding(
     padding: const EdgeInsets.only(right: 20, left: 20),
     child: SizedBox(
       child: Text(text,
@@ -43,5 +39,5 @@ Widget createAboutText(String text) {
             fontWeight: FontWeight.bold,
           )),
     ),
-  );
+  ));
 }
